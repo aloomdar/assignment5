2ed0384b384e79adf20bdff6df1b857c99782f43
aloomdar <aqanoongo@gmail.com>
2024-11-20 18:11:22 -0500

    commit 3

--- src/Child1.js
+++ src/Child1.js
@@ -5,7 +5,7 @@ import * as d3 from "d3";
 class Child1 extends Component {
   state = {
     company: "Apple", // Default Company
-    selectedMonth: "November", //Default Month
+    selectedMonth: 11, // Default Month (November, 1-based index)
   };
 
   changeMonth = (e) => {
@@ -17,93 +17,92 @@ class Child1 extends Component {
   };
 
   componentDidMount() {
-    console.log("Data: ", this.props.csv_data); // Use this data as default. When the user will upload data this props will provide you the updated data
+    this.drawChart(this.props.csv_data);
+  }
 
-    const data = this.props.csv_data;
-    data.forEach((item) => {
-      item.Date = new Date(item.Date);
-    });
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.csv_data !== this.props.csv_data ||
+      prevState.company !== this.state.company ||
+      prevState.selectedMonth !== this.state.selectedMonth
+    ) {
+      this.drawChart(this.props.csv_data);
+    }
+  }
+
+  drawChart(data) {
+    const filteredData = data.filter(
+      (d) =>
+        d.Company === this.state.company &&
+        d.Date.getMonth() === this.state.selectedMonth - 1
+    );
 
     const margin = { top: 20, right: 30, bottom: 40, left: 40 },
-      width = 1200,
-      height = 900,
-      innerWidth = 1200 - margin.left - margin.right,
-      innerHeight = 900 - margin.top - margin.bottom;
+      width = 1000,
+      height = 500,
+      innerWidth = width - margin.left - margin.right,
+      innerHeight = height - margin.top - margin.bottom;
+
+    d3.select(".mysvg").selectAll("*").remove();
 
     const svg = d3
       .select(".mysvg")
       .attr("width", width)
       .attr("height", height)
-      .select("g")
+      .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
     const xScale = d3
       .scaleTime()
-      .domain(d3.extent(data, (d) => d.Date))
-      .range([0, width]);
+      .domain(d3.extent(filteredData, (d) => d.Date))
+      .range([0, innerWidth]);
+
     const yScale = d3
       .scaleLinear()
-      .domain([d3.min(data, (d) => d.Open), d3.max(data, (d) => d.Open)])
-      .range([height, 0]);
+      .domain([
+        d3.min(filteredData, (d) => Math.min(d.Open, d.Close)),
+        d3.max(filteredData, (d) => Math.max(d.Open, d.Close)),
+      ])
+      .range([innerHeight, 0]);
 
-    var openLine = d3
+    const lineOpen = d3
       .line()
-      .curve(d3.curveCardinal)
       .x((d) => xScale(d.Date))
-      .y((d) => yScale(d.Open));
-    var pathData1 = openLine(data);
+      .y((d) => yScale(d.Open))
+      .curve(d3.curveCardinal);
 
-    var closeLine = d3
+    const lineClose = d3
       .line()
-      .curve(d3.curveCardinal)
       .x((d) => xScale(d.Date))
-      .y((d) => yScale(d.Close));
-    var pathData2 = closeLine(data);
+      .y((d) => yScale(d.Close))
+      .curve(d3.curveCardinal);
 
     svg
-      .selectAll("path")
-      .data([pathData1])
-      .join("path")
-      .attr("d", (d) => d)
+      .append("path")
+      .datum(filteredData)
+      .attr("d", lineOpen)
       .attr("fill", "none")
-      .attr("stroke", "blue");
+      .attr("stroke", "#b2df8a")
+      .attr("stroke-width", 2);
+
     svg
-      .selectAll("path1")
-      .data([pathData2])
-      .join("path")
-      .attr("d", (d) => d)
+      .append("path")
+      .datum(filteredData)
+      .attr("d", lineClose)
       .attr("fill", "none")
-      .attr("stroke", "red");
+      .attr("stroke", "#e41a1c")
+      .attr("stroke-width", 2);
 
     svg
-      .selectAll(".x.axis")
-      .data([null])
-      .join("g")
-      .attr("class", "x axis")
-      .attr("transform", `translate(0, ${innerHeight})`)
-      .call(d3.axisBottom(xScale));
-    svg
-      .selectAll(".y.axis")
-      .data([null])
-      .join("g")
-      .attr("class", "y axis")
-      .call(d3.axisLeft(yScale));
-  }
+      .append("g")
+      .attr("transform", `translate(0, ${innerHeight + 10})`)
+      .call(d3.axisBottom(xScale).ticks(10));
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.csv_data !== this.props.csv_data){
-      const data = this.props.csv_data;
-      data.forEach((item) => {
-        item.Date = new Date(item.Date);
-      });
-      
-    }
+    svg.append("g").call(d3.axisLeft(yScale));
   }
-
-  
 
   render() {
-    const options = ["Apple", "Microsoft", "Amazon", "Google", "Meta"]; // Use this data to create radio button
+    const options = ["Apple", "Microsoft", "Amazon", "Google", "Meta"];
     const months = {
       January: 1,
       February: 2,
@@ -117,7 +116,7 @@ class Child1 extends Component {
       October: 10,
       November: 11,
       December: 12,
-    }; // Use this data to create dropdown
+    };
 
     return (
       <div className="child1">
@@ -151,9 +150,7 @@ class Child1 extends Component {
             ))}
           </select>
         </div>
-        <svg className="mysvg">
-          <g></g>
-        </svg>
+        <svg className="mysvg"></svg>
       </div>
     );
   }
